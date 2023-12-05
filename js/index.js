463436ee29af5bb60c4e886e7c6fa4d5c32859c9
Niko Cremer <n.ko@posteo.de>
2023-12-05 10:12:22 +0100

    fill function

--- js/index.js
+++ js/index.js
@@ -2,6 +2,42 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log("response", response);
+  console.log("data", data);
+}
 
 fetchData();
+
+/*
+
+async function fetchJoke() {
+const response = await fetch("url")
+
+const data = await response.json();
+
+console.log(response);
+}
+
+fetchJoke()
+
+
+Have a look at the [`js/index.js`](./js/index.js) file: There is an `url` variable and an empty `fetchData()` function.
+
+Inside of the `fetchData()` function, use the `url` variable to fetch data from the Star Wars API.
+
+Log the fetched data to the console and play around with it:
+
+- log the entire data object
+- log different values of the data object
+- Bonus: Can you log the eye color of R2-D2?
+
+The following hints may guide you:
+
+- make sure to wait for all asynchronous operations
+
+Please switch to the [`js/index.js`](./js/index.js) file: May the force be with you!
+*/
